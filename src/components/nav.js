955648f9b648b10cecd48b921bacea136082aa4d
Sam <climbinsheep@gmail.com>
2017-02-27 10:17:58 +0000

    adding nav-collapsed functionality

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,24 +1,46 @@
 import React from "react";
-import { Link } from "react-router";
 import "../index.css";
 
 export default class Nav extends React.Component {
 
+ 
+  toggleMenue(){
+    var menueShow = document.querySelector("#nav-open");
+    menueShow.style.display = (menueShow.style.display === 'none' || menueShow.style.display === '')? 'flex' : "none";
+  }
+  
+  handleResize(){
+    var menueShow = document.querySelector("#nav-open");
+    menueShow.style.display = 'none';
+  }
+  
+  componentDidMount(){
+    window.addEventListener('resize', this.handleResize);
+  }
+  
+
   render(){
     return (
-      <div className="navbar navbardefalt">
-        <Link id="siteTitle" to="home">Sam Williams</Link>
-        <div id="nav-links">
-          <Link className="menueBtn" to="about">About</Link>
-          <Link className="menueBtn" to="projects">Projects</Link>
-          <Link className="menueBtn" to="contact">Contact Me</Link>
+      <div>
+        <div id="navbar">
+          <a href="#about" id="siteTitle">Sam Williams</a>
+          <div id="nav-links">
+            <a href="#about"><button className="menueBtn">About</button></a>
+            <a href="#projects"><button className="menueBtn">Projects</button></a>
+            <a href="#contact"><button className="menueBtn">Contact Me</button></a>
+          </div>
+          <div id="nav-collapsed">
+            <button className="menueBtn" id="exp" onClick={this.toggleMenue}>
+              <div className="menstrip exp1"></div>
+              <div className="menstrip exp2"></div>
+              <div className="menstrip exp3"></div>
+            </button>
+          </div>
         </div>
-        <div id="nav-collapsed">
-          <button className="menueBtn" id="exp">
-            <div className="menstrip exp1"></div>
-            <div className="menstrip exp2"></div>
-            <div className="menstrip exp3"></div>
-          </button>
+        <div id="nav-open">
+          <a href="#about"><button className="menueBtn">About</button></a>
+          <a href="#projects"><button className="menueBtn">Projects</button></a>
+          <a href="#contact"><button className="menueBtn">Contact Me</button></a>
         </div>
       </div>
     );
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,16 +6,16 @@ export default class Layout extends React.Component {
   render(){
     return (
       <div className="about">
+        <a class="anchor" name="about" id="aboutAnchor"></a>
         <div id="root"></div>
         <h1 className="sectionTitle">About Me</h1>
         <div className="twocols">
-          <div class="aboutText">
+          <div className="aboutText">
             <h4 className="text-center"> Front end developer and UX/UI designer, with experience in Data analytics, visualisation, machine learning and project management. Always striving to learn and develop. </h4>
-            <hr></hr>
             <h3 className="text-center"> Web Developer - UX Designer - Data Scientist</h3>
           </div>
           <div className="profpic">
-            <img className="img-circle img-responsive center-block" alt="Sam Williams" src="http://climbingstrengthtraining.com/wp-content/uploads/2017/01/samImg.jpg" />
+            <img className="img-circle img-responsive" alt="Sam Williams" src="http://climbingstrengthtraining.com/wp-content/uploads/2017/01/samImg.jpg" />
           </div>
         </div>
         <div className="Skills">
